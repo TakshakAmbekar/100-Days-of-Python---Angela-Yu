b2972a4b7a98ffe1ae5b4a8ac83c66d9159c005f
Takshak Ambekar <ambekartakshak@gmail.com>
2026-07-30 08:12:01 +0530

    Day 1

---
+++ Day 1/1. print.py
@@ -0,0 +1,22 @@
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+
+print("")       # adds a single empty line
+
+print("Hello World!\nHello World!\nHello World!")
+
+print("\n")     # adds two empty lines
+
+print('''Hello World!
+Hello World!
+Hello World!''')
+
+print()         # adds a single empty line
+
+
+# STRING CONCATENATION
+
+name = "Takshak"
+
+print("Hello " + name)
---
+++ Day 1/2. input.py
@@ -0,0 +1,2 @@
+name = input("What is your name? ")
+print("Hello", name)                    # Automatically add a space in between
