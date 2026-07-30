0bf279fef7517322d948b4ab9f82c92274853d07
Takshak Ambekar <ambekartakshak@gmail.com>
2026-07-30 10:57:58 +0530

    Day 2

---
+++ Day 2/4. Number Manipulation and F Strings.py
@@ -0,0 +1,21 @@
+# round(float, decimal_places)
+print(round(3.123456789, 2))
+
+'''
+Number Manipulation
++= 
+-= 
+*= 
+/= 
+//= 
+**=
+'''
+
+# f-string
+score = 10
+height = 1.8
+is_winning = True
+
+print(f"Your score is {score}\nYour height is {height}\nYou are winning is {is_winning}")
+
+print(6 + 4 // 2 - 2.0)
