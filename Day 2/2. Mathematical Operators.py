e1f02599e8f585aac9fe31b9b6a064cf2f45224c
Takshak Ambekar <ambekartakshak@gmail.com>
2026-08-05 22:52:42 +0530

    Day 2 - Datatypes

--- Day 2/2. Mathematical Operators.py
+++
@@ -1,8 +0,0 @@
-print("My age: " + str(25))
-print(123 + 456)
-print(7 - 3)
-print(2 * 3)    
-print(8 / 3)    # result is a float even if there's a clean division using single slash 
-print(8 // 3)   # double slash rounds down to nearest integer
-print(2 ** 3)   # exponentiation
-print(8 % 3)    # modulo returns the remainder after division
--- Day 2/3. BMI Calculator.py
+++
@@ -1,6 +0,0 @@
-# BMI equals weight in kg divided by square of height in m
-height = 1.65
-weight = 60
-bmi = weight / (height ** 2)
-print("Your BMI is:", round(bmi, 2))
-
