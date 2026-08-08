80c6d91f55bd4b139a37f7f3871a5996c9197182
Takshak Ambekar <ambekartakshak@gmail.com>
2026-08-09 00:29:44 +0530

    Day 18 - GUI

--- Day 18 - GUI/Dot Art.py
+++ Day 18 - GUI/Dot Art.py
@@ -3,34 +3,34 @@ import random
 import colorgram
 
 colors = colorgram.extract("Day 18 - GUI/hirst_dot_painting.jpg", 10)
-print(colors[0].rgb)
-# timmy = Turtle()
 
-# timmy.speed(0)
-# timmy.pensize(10)
-# timmy.penup()
-# timmy.setposition(-200, 200)
-# colormode(255)
+timmy = Turtle()
+
+timmy.speed(0)
+timmy.pensize(10)
+timmy.penup()
+timmy.setposition(-200, 200)
+colormode(255)
 
 
-# def choose_color():
-#     timmy.pencolor(random.choice(colors).rgb)
+def choose_color():
+    timmy.pencolor(random.choice(colors).rgb)
     
-# def horizontal():
-#     for _ in range(10):
-#         choose_color()
-#         timmy.dot()
-#         timmy.forward(40)
+def horizontal():
+    for _ in range(10):
+        choose_color()
+        timmy.dot()
+        timmy.forward(40)
 
-# def vertical():
-#     horizontal()
-#     x,y = timmy.position()
-#     timmy.setposition(-200, y - 40)
+def vertical():
+    horizontal()
+    x,y = timmy.position()
+    timmy.setposition(-200, y - 40)
     
 
-# for _ in range(10):
-#     vertical()
+for _ in range(10):
+    vertical()
 
 
-# screen = Screen()
-# screen.exitonclick()
+screen = Screen()
+screen.exitonclick()
